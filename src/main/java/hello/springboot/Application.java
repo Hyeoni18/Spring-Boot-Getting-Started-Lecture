47e7f2b22c23dfa0b76895872a8821071f8c64c7
Hyeoni18 <gusgnl105@gmail.com>
2022-02-08 15:48:23 +0900

    자동 설정 만들기 2부: @ConfigurationProperties

--- src/main/java/hello/springboot/Application.java
+++ src/main/java/hello/springboot/Application.java
@@ -14,15 +14,4 @@ public class Application {
         application.setWebApplicationType(WebApplicationType.NONE);
         application.run(args);
     }
-
-    //문제점 재현
-    @Bean
-    public Holoman holoman() {
-        Holoman holoman = new Holoman();
-        holoman.setName("holong");
-        holoman.setHowLong(10);
-        return holoman;
-    }
-    //bean 등록 순서가 있음. 1차 componentScan 등록, 먼저 위 bean 등록을 함. 이후 autoConfiguration bean 등록. overriding 되면 위 bean은 덮여 사라짐.
-    //스프링부트 2.1 부터는 동작 방법이 달라져 overriding error 발생. spring.main.allow-bean-definition-overriding=true setting 을 하든, @ConditionalOnMissingBean 어노테이션을 자동 설정 제공하는 쪽에 추가하면 됨.
 }

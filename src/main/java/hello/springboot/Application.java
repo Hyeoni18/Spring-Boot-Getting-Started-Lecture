bc9c661b2fb7941f65365b7c0caf2f4353769409
Hyeoni18 <gusgnl105@gmail.com>
2022-02-08 23:37:16 +0900

    외부 설정 2부

--- src/main/java/hello/springboot/Application.java
+++ src/main/java/hello/springboot/Application.java
@@ -1,21 +1,23 @@
 package hello.springboot;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-
-import java.io.PrintStream;
 
 @SpringBootApplication
 public class Application {
 
+    @ConfigurationProperties("server")
+    @Bean
+    public ServerProperties serverProperties() {
+        return new ServerProperties();
+    }
+
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
-  //      app.addListeners(new SampleListener());
         app.setWebApplicationType(WebApplicationType.NONE);
         app.run(args);
     }
--- src/main/java/hello/springboot/SampleListener.java
+++ src/main/java/hello/springboot/SampleListener.java
@@ -1,5 +1,6 @@
 package hello.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -13,17 +14,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleListener implements ApplicationRunner {
 
-    @Value("${boot.fullName}")
-    private String name;
-
-    @Value("${boot.count}")
-    private int count;
+    @Autowired
+    BootProperties properties;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("============================");
-        System.out.println(name);
-        System.out.println(count);
+        System.out.println(properties.getFullName());
+        System.out.println(properties.getCount());
+        System.out.println(properties.getSessionTimeout());
         System.out.println("============================");
     }
 }

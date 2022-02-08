f2578ca4352f08c37adc975e0502ef538e57d4bb
Hyeoni18 <gusgnl105@gmail.com>
2022-02-08 16:41:19 +0900

    내장 웹 서버 응용 1부: 컨테이너와 포트

--- src/main/java/hello/springboot/Application.java
+++ src/main/java/hello/springboot/Application.java
@@ -9,9 +9,6 @@ import org.springframework.context.annotation.Bean;
 public class Application {
 
     public static void main(String[] args) {
-        //SpringApplication.run(Application.class, args);
-        SpringApplication application = new SpringApplication(Application.class);
-        application.setWebApplicationType(WebApplicationType.NONE);
-        application.run(args);
+        SpringApplication.run(Application.class, args);
     }
 }

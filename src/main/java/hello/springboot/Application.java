f1fcfbdb691613ebb486790f44080e3fdb92ab6a
Hyeoni18 <gusgnl105@gmail.com>
2022-02-08 18:34:09 +0900

    SpringApplication 1부

--- src/main/java/hello/springboot/Application.java
+++ src/main/java/hello/springboot/Application.java
@@ -1,14 +1,21 @@
 package hello.springboot;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder()
+                .sources(Application.class)
+                .run(args);
     }
 }

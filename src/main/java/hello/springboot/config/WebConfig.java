e0ef2980f19ea484fcd18c8a71916a87a0496852
Hyeoni18 <gusgnl105@gmail.com>
2022-02-09 23:40:26 +0900

    스프링 웹 MVC 1부: 소개

---
+++ src/main/java/hello/springboot/config/WebConfig.java
@@ -0,0 +1,11 @@
+package hello.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+
+}

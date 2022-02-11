04a76fbb9d8b26a6de2137ae5ffc333782bbb183
Hyeoni18 <gusgnl105@gmail.com>
2022-02-11 14:34:27 +0900

    스프링 REST 클라이언트 2부: 커스터마이징

--- src/main/java/hello/springboot/Application.java
+++ src/main/java/hello/springboot/Application.java
@@ -2,6 +2,12 @@ package hello.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +16,25 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    //전역설정
+    @Bean
+    public WebClientCustomizer webClientCustomizer() {
+        //WebClient.Builder 자체를 등록하는 방법도 있음.
+        return new WebClientCustomizer() {
+            @Override
+            public void customize(WebClient.Builder webClientBuilder) {
+                webClientBuilder.baseUrl("http://localhost:8080");
+            }
+        };
+    }
+
+    @Bean
+    public RestTemplateCustomizer restTemplateCustomizer() {
+        return new RestTemplateCustomizer() {
+            @Override
+            public void customize(RestTemplate restTemplate) {
+                restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory()); //아파치 http client 를 사용하게 됨. 자바의 기본 http 커넥션을 사용하지 않음.
+            }
+        };
+    }
 }
--- src/main/java/hello/springboot/RestRunner.java
+++ src/main/java/hello/springboot/RestRunner.java
@@ -22,54 +22,14 @@ public class RestRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        RestTemplate restTemplate = restTemplateBuilder.build();
+        WebClient webClient = builder
+      //          .baseUrl("http://localhost:8080")
+                .build();
 
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
-        //동기
-        //TODO /hello 호출
-        String helloResult = restTemplate.getForObject("http://localhost:8080/hello", String.class);
-        System.out.println("R : "+helloResult);
-        //TODO /world 호출
-        String worldResult = restTemplate.getForObject("http://localhost:8080/world", String.class);
-        System.out.println("R : "+worldResult);
-
-        stopWatch.stop();
-        System.out.println("R : "+stopWatch.prettyPrint());
-
-        WebClient webClient = builder.build();
-        stopWatch.start();
-
-        //비동기
-        //Webflux와 Mono에 대한 학습이 필요. 대충 Streaming API 인데. 스트림을 서브스크라이브 하기 전까지는 스트림이 흐르지 않음. 물이 고여있다 생각하면 됨.
-        Mono<String> helloMono = webClient.get().uri("http://localhost:8080/hello").retrieve().bodyToMono(String.class);
-        //칸막이를 열어줘야 흘러. 열어주자.
+        Mono<String> helloMono = webClient.get().uri("/hello").retrieve().bodyToMono(String.class); //간단하게 호출 가능.
         helloMono.subscribe(s -> {
             System.out.println(s);
-
-            if(stopWatch.isRunning()) {
-                stopWatch.stop();
-            }
-
-            System.out.println(stopWatch.prettyPrint());
-            stopWatch.start();
         });
-
-        Mono<String> worldMono = webClient.get().uri("http://localhost:8080/world").retrieve().bodyToMono(String.class);
-        worldMono.subscribe(s -> {
-            System.out.println(s);
-
-            if(stopWatch.isRunning()) {
-                stopWatch.stop();
-            }
-
-            System.out.println(stopWatch.prettyPrint());
-            stopWatch.start();
-        });
-
-        stopWatch.stop();
-        System.out.println(stopWatch.prettyPrint());
-
     }
 
 }

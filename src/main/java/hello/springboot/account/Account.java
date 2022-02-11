f9630c583122c23a95561329cf0f6a5c37f446c8
Hyeoni18 <gusgnl105@gmail.com>
2022-02-11 10:34:39 +0900

    스프링 데이터 9부: Redis

--- src/main/java/hello/springboot/account/Account.java
+++ src/main/java/hello/springboot/account/Account.java
@@ -1,46 +1,23 @@
 package hello.springboot.account;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import java.util.Objects;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
-@Entity
+@RedisHash("account")
 public class Account {
+
     @Id
-    @GeneratedValue //자동 생성
-    private Long id;
+    private String id;
 
     private String username;
 
-    private String password;
-
     private String email;
 
-    private boolean active;
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -52,24 +29,11 @@ public class Account {
         this.username = username;
     }
 
-    public String getPassword() {
-        return password;
+    public String getEmail() {
+        return email;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Account account = (Account) o;
-        return Objects.equals(id, account.id) && Objects.equals(username, account.username) && Objects.equals(password, account.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, username, password);
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
--- src/main/java/hello/springboot/account/AccountRepository.java
+++ src/main/java/hello/springboot/account/AccountRepository.java
@@ -1,12 +1,7 @@
 package hello.springboot.account;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
+public interface AccountRepository extends CrudRepository<Account, String> {
 
-public interface AccountRepository extends JpaRepository<Account, Long> {
-
-    //@Query(nativeQuery = true, value = "select * from account where username='{0}'")
-    Optional<Account> findByUsername(String username);
 }

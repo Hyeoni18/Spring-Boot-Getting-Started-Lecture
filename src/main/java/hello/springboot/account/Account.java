da4fdb3f49bc902324fe0234517c9b216a5dbf4f
Hyeoni18 <gusgnl105@gmail.com>
2022-02-11 11:04:01 +0900

    스프링 데이터 10부: MongoDB

--- src/main/java/hello/springboot/account/Account.java
+++ src/main/java/hello/springboot/account/Account.java
@@ -1,9 +1,9 @@
 package hello.springboot.account;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@RedisHash("account")
+@Document(collation = "accounts")
 public class Account {
 
     @Id
